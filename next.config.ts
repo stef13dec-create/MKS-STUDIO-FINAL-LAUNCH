649dac65e389d384a0108abe60a626c05b3fa722
stef13dec-create <stefan@example.com>
2026-03-24 21:07:11 +0200

    chore: remove basePath from next.config.ts and static_site_generator from workflow

--- next.config.ts
+++ next.config.ts
@@ -9,7 +9,6 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   output: 'export',
-  basePath: process.env.NODE_ENV === 'production' ? '/MKS-STUDIO-FINAL-LAUNCH' : '',
   images: {
     unoptimized: true,
     remotePatterns: [
